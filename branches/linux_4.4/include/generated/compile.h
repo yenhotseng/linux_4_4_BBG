9ca7d7ece7d607e1f3e46508921dd88fee6d54b9
yenho.tseng <yenho.tseng@4a28fb1a-7624-11e5-9650-47106e661c56>
2018-10-22 11:27:16 +0000

    Port Linux 4.4.113 to VT-BUDs.

---
+++ branches/linux_4.4/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 10 */
+/* SMP */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#10 SMP Mon Oct 22 17:03:44 CST 2018"
+#define LINUX_COMPILE_BY "yenho"
+#define LINUX_COMPILE_HOST "ubuntu18"
+#define LINUX_COMPILER "gcc version 7.3.0 (Ubuntu/Linaro 7.3.0-27ubuntu1~18.04) "
